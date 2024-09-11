570141336b0726b2691e961562129fcfb830cb71
akshay <akshay_111meher@hotmail.com>
2024-09-11 18:31:11 +0400

    refactor

--- src/config.ts
+++ src/config.ts
@@ -6,32 +6,39 @@ dotenv.config()
 const envSchema = z.object({
   RPC: z.string().url(),
   PRIVATE_KEY: z.string(),
-  PORT: z.number(),
+  PORT: z.coerce.number(),
   API_KEY: z.string(),
   SERVER_MODE: z.enum(['DEV', 'PROD']),
-  PROOF_REWARD: z.number(),
-  MARKET_ID: z.number(),
+  PROOF_REWARD: z.coerce.number(),
+  MARKET_ID: z.coerce.number(),
   MATCHING_ENGINE_URL: z.string().url(),
   CHECK_INPUT_URL: z.string().url(),
-  REDIS_HOST: z.string().url(),
-  REDIS_PORT: z.number(),
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.coerce.number(),
   REDIS_PASSWORD: z.string(),
-  RATE_LIMIT_WINDOW: z.number().default(10),
-  MAX_REQUESTS: z.number().default(10),
-  THROTTLE_DELAY: z.number().default(10),
-  ASSIGNMENT_DELAY: z.number().default(10000000000),
+  RATE_LIMIT_WINDOW: z.coerce.number().default(10),
+  MAX_REQUESTS: z.coerce.number().default(10),
+  THROTTLE_DELAY: z.coerce.number().default(10),
+  ASSIGNMENT_DELAY: z.coerce.number().default(10000000000),
   PROOF_GENERATION_DELAY: z.number().default(10000000000),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
 
 if (!parsedEnv.success) {
-  const missingEnvVars = Object.keys(parsedEnv.error.format())
-    .map((key) => key)
-    .join(', ')
+  const formattedErrors = parsedEnv.error.format()
 
-  console.error(`Missing or invalid environment variables: ${missingEnvVars}`)
-  throw new Error(`Missing or invalid environment variables: ${missingEnvVars}`)
+  Object.entries(formattedErrors).forEach(([key, error]) => {
+    if (Array.isArray(error)) {
+      console.error(`❌ Error for ${key}: ${error.join(', ')}`)
+    } else if (error && '_errors' in error) {
+      console.error(`❌ Error for ${key}: ${error._errors.join(', ')}`)
+    }
+  })
+
+  throw new Error(
+    'Some environment variables are missing or invalid. Please check the log for details.'
+  )
 }
 
 const config = parsedEnv.data
--- src/controllers/proverController.ts
+++ src/controllers/proverController.ts
@@ -1,4 +1,3 @@
-import { KalypsoSdk } from 'kalypso-sdk'
 import { ethers } from 'ethers'
 import BigNumber from 'bignumber.js'
 import { PublicAndSecretInputPair } from 'kalypso-sdk/dist/types'
@@ -6,23 +5,12 @@ import { PublicAndSecretInputPair } from 'kalypso-sdk/dist/types'
 import config from '../config'
 import { checkRateLimitAndThrottle } from '../middleware'
 
-const kalypsoConfig = {
-  payment_token: '0x01d84D33CC8636F83d2bb771e184cE57d8356863',
-  staking_token: '0xdb69299dDE4A00c99b885D9f8748B2AeD1Fe4Ed4',
-  mock_attestation_verifier: '0x1dC40628443D93eA82945a9206e0b527BA3EA028',
-  attestation_verifier: '0x63EEf1576b477Aa60Bfd7300B2C85b887639Ac1b',
-  transfer_verifier_wrapper: '0x30A5fFf0D0d54fab407a409467835e56830a7471',
-  zkb_verifier_wrapper: '0xeE89C22838a691d03fB3b6f47C387d06917C0bBD',
-  entity_registry: '0x002064F4d224EA4F90e9A74B5fE3f3812886c6DF',
-  generator_registry: '0x5ce3e1010028C4F5687356D721e3e2B6DcEA7C25',
-  dispute: '0x41fD92A4Bc74F8B9f46e1fA0825C40aeD5AFDb92',
-  proof_market_place: '0x0b6340a893B944BDc3B4F012e934b724c83abF97',
-  priority_list: '0xfd94a9Aed9d5f8f00b70A1908378fcc1C4a04B4E',
-  input_and_proof_format: '0xBcBaccBA21D0F2089029a3184BcB612e5aFF7911',
-  tee_verifier_deployer: '0x5acCC2F599045D13EA03e4c2b7b0Ed9F8C7Fb99C',
-  checkInputUrl: config.CHECK_INPUT_URL,
-  attestationVerifierEndPoint: 'http://13.201.207.60:1400',
-}
+import {
+  latestBlock,
+  kalypso,
+  walletAddress,
+  getTransactionReceipt,
+} from '../kalypso'
 
 type createAskAndGetProofParams = {
   pub: any
@@ -50,24 +38,16 @@ const createAskAndGetProof = async (
     let input = createAskAndGetProofParams.pub
     let secret = createAskAndGetProofParams.sec
 
-    const provider = new ethers.JsonRpcProvider(config.RPC)
-    const wallet = new ethers.Wallet(config.PRIVATE_KEY, provider)
-
-    console.log('using address', await wallet.getAddress())
-
     let abiCoder = new ethers.AbiCoder()
 
     let inputBytes = abiCoder.encode(['string[1]'], [[input]])
 
-    const kalypso = new KalypsoSdk(wallet, kalypsoConfig)
-
     const secretString = JSON.stringify(secret)
 
-    const latestBlock = await provider.getBlockNumber()
-    const assignmentDeadline = new BigNumber(latestBlock).plus(
+    const assignmentDeadline = new BigNumber(await latestBlock()).plus(
       config.ASSIGNMENT_DELAY
     )
-    const proofGenerationTimeInBlocks = new BigNumber(latestBlock).plus(
+    const proofGenerationTimeInBlocks = new BigNumber(await latestBlock()).plus(
       config.PROOF_GENERATION_DELAY
     )
 
@@ -78,7 +58,7 @@ const createAskAndGetProof = async (
       config.PROOF_REWARD,
       assignmentDeadline.toFixed(0),
       proofGenerationTimeInBlocks.toFixed(0),
-      await wallet.getAddress(),
+      await walletAddress(),
       0, // TODO: keep this 0 for now
       Buffer.from(secretString),
       false
@@ -86,8 +66,7 @@ const createAskAndGetProof = async (
     await askRequest.wait()
     console.log('Ask Request Hash: ', askRequest.hash)
 
-    let receipt = await provider.getTransactionReceipt(askRequest.hash)
-
+    let receipt = await getTransactionReceipt(askRequest.hash)
     let askId = await kalypso.MarketPlace().getAskId(receipt!)
     console.log('Ask ID :', askId)
 
@@ -170,8 +149,6 @@ const createEncryptedAskAndGetProof = async (
 ) => {
   const provider = new ethers.JsonRpcProvider(config.RPC)
   const wallet = new ethers.Wallet(config.PRIVATE_KEY, provider)
-
-  const kalypso = new KalypsoSdk(wallet, kalypsoConfig)
 
   const isValid = await kalypso
     .MarketPlace()
@@ -292,17 +269,4 @@ export const proveTransaction = async (req: any, res: any) => {
       res.status(500).send('Internal Server Error')
     }
   }
-}
-
-// Define the CheckInputResponse interface
-interface CheckInputResponse {
-  valid: boolean
-}
-// Define the EncryptedInputPayload interface
-interface EncryptedInputPayload {
-  acl: Uint8Array
-  public_inputs?: Uint8Array
-  encrypted_secrets: Uint8Array
-  me_decryption_url: string
-  market_id: string
 }
